b25995f525e6a71adfe65df88cf0d1988e39a613
welisonmenezes <welisonmenezes@hotmail.com>
2021-05-13 20:09:06 -0300

    Aula 1

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace webapp
+namespace Trilha_Jr_1
 {
     public class Program
     {
